859f47dd687d7fd8cb0b2449544eb6317c40d62d
Uchio KONDO <udzura@pepabo.com>
2017-11-20 11:58:06 +0900

    Fix styles for mobile, add staffs

--- fukuoka02/assets/js/index.js
+++ fukuoka02/assets/js/index.js
@@ -1,10 +1,24 @@
+// smooth scroll
 $(function () {
-  $('a[href^=#]').click(function () {
+  $('a[href^=#]').on("click", function () {
     var speed = 400,
       href = $(this).attr("href"),
       target = $(href === "#" || href === "" ? 'html' : href),
       position = target.offset().top;
     $("html, body").animate({scrollTop: position}, speed, "swing");
     return false;
+  });
+  // hamburger menu
+  $(".js-hamburger").on("click", function () {
+    $(".js-nav").slideToggle(function () {
+      $(".js-hamburger").hide();
+      $(".js-close").show();
+    });
+  });
+  $(".js-close").on("click", function () {
+    $(".js-nav").slideToggle(function () {
+      $(".js-close").hide();
+      $(".js-hamburger").show();
+    });
   });
 });
