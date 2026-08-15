7c854e70c72ce8270b1b42b1a79085f8a7d3b7de
Shintaro Kakutani <shintaro@kakutani.com>
2026-08-15 19:07:04 +0900

    Open only off-site event links in new tabs

--- _plugins/event_filters.rb
+++ _plugins/event_filters.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 require "date"
+require "uri"
 
 module EventFilters
   def sort_events_by_start_on(events)
     Array(events).sort_by do |event|
       Date.iso8601(event.fetch("start_on").to_s)
     end
+  end
+
+  def external_to_regional_rubykaigi(url)
+    host = URI.parse(url.to_s).host
+    !host.nil? && host != "regional.rubykaigi.org"
+  rescue URI::InvalidURIError
+    false
   end
 end
 
